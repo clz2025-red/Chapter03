d8df48c12812de4d22b9050712e313a03517f2e7
clz2025-red <clz2025.red@gmail.com>
2025-05-09 16:44:23 +0900

    2025-05-09

---
+++ src/com/javaex/ex02/Point.java
@@ -0,0 +1,43 @@
+package com.javaex.ex02;
+
+public class Point extends Object { //상속생략가능
+
+	// 필드
+	private int x;
+	private int y;
+
+	// 생성자
+	public Point() {
+		super(); // Object클래스의 기본생성자 호출 생략가능
+	}
+
+	public Point(int x, int y) {
+		super(); // Object클래스의 기본생성자 호출 생략가능
+		this.x = x;
+		this.y = y;
+	}
+
+	// 메소드 gs
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+	
+}
---
+++ src/com/javaex/ex04/Rectangle.java
@@ -0,0 +1,41 @@
+package com.javaex.ex04;
+
+public class Rectangle { // 기본값 누구를 상속받는지?
+
+	// 필드
+	private int width;
+	private int height;
+
+	// 생성자
+	public Rectangle() {
+		super(); //
+	}
+
+	public Rectangle(int width, int height) {
+		super(); //
+		this.width = width;
+		this.height = height;
+	}
+
+	//메소드 gs
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	//메소드 일반
+	
+	
+
+}
